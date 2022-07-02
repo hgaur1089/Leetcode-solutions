20e7a5835c90533a1f5fcc13d269759cf3ffc979
hgaur1089 <60226963+hgaur1089@users.noreply.github.com>
2022-07-02 20:05:23 +0530

    Time: 8 ms (89.04%), Space: 8.3 MB (57.90%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -1,21 +1,37 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        unordered_map<char, bool> mpp;
-        int len = 0;
-        int l = 0, r = 0;
+//         unordered_map<char, bool> mpp;
+//         int len = 0;
+//         int l = 0, r = 0;
         
-        while(r < s.length()){
-            if(mpp.count(s[r]) > 0){
-                while(mpp.count(s[r]) > 0){
-                    mpp.erase(s[l]);
-                    l++;
-                }
-            } else {
-                len = max(len, r-l+1);
-                mpp[s[r]] = true;
-                r++;
+//         while(r < s.length()){
+//             if(mpp.count(s[r]) > 0){
+//                 while(mpp.count(s[r]) > 0){
+//                     mpp.erase(s[l]);
+//                     l++;
+//                 }
+//             } else {
+//                 len = max(len, r-l+1);
+//                 mpp[s[r]] = true;
+//                 r++;
+//             }
+//         }
+        
+//         return len;
+        vector<int> map(256, -1);
+        int left = 0, right = 0;
+        int size = s.size();
+        int len = 0;
+        
+        while(right < size){
+            if(map[s[right]] != -1){
+                left = max(left, map[s[right]] + 1);
             }
+            
+            map[s[right]] = right;
+            len = max(right-left+1, len);
+            right++;
         }
         
         return len;
