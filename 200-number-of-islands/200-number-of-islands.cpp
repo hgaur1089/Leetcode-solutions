61325691e54d318b6ef7ac96c6ebaec91621ef57
hgaur1089 <60226963+hgaur1089@users.noreply.github.com>
2022-07-20 21:31:42 +0530

    Time: 83 ms (15.02%), Space: 18.2 MB (16.26%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,7 +1,45 @@
 class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
-        return solveDFS(grid);
+        return solveBFS(grid);
+    }
+    
+    int solveBFS(vector<vector<char>>& grid){
+        int n = grid.size(); 
+        int m = grid[0].size();
+        int ans = 0;
+        
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(grid[i][j] == '1'){
+                    ans++;
+                    grid[i][j] = '0';
+                    
+                    queue<pair<int, int>> q;
+                    q.push({i, j});
+                    while(!q.empty()){
+                        int i = q.front().first;
+                        int j= q.front().second;
+                        q.pop();
+                        
+                        removeIsland(grid, i+1, j, q);
+                        removeIsland(grid, i-1, j, q);
+                        removeIsland(grid, i, j+1, q);
+                        removeIsland(grid, i, j-1, q);
+                        
+                    }
+                }
+            }
+        }
+        
+        return ans;
+    }
+    
+    void removeIsland(vector<vector<char>>& grid, int i, int j, queue<pair<int, int>>& q){
+        if(i>=0 && i<grid.size() && j>=0 && j<grid[0].size() && grid[i][j] == '1'){
+            grid[i][j] = '0';
+            q.push({i, j});
+        }
     }
     
     int solveDFS(vector<vector<char>>& grid){
