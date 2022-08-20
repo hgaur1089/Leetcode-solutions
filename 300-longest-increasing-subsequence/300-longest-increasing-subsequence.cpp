bb447defa5e1763285e5385b0f39e10f54986bcc
hgaur1089 <60226963+hgaur1089@users.noreply.github.com>
2022-08-20 22:25:24 +0530

    Time: 469 ms (35.67%), Space: 10.7 MB (24.85%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,25 +1,26 @@
 class Solution {
+    int lis2(int n, vector<int>& arr){
+        vector<int> next(n+1, 0);
+        vector<int> curr(n+1, 0);
+
+        for(int index=n-1; index>=0; index--){
+            for(int prevLen=index; prevLen>=0; prevLen--){
+                int prevIndex = prevLen-1;
+                int len = 0 + next[prevLen]; //not take
+
+                if(prevIndex==-1 || arr[index]>arr[prevIndex]){
+                    len = max(len, 1 + next[index+1]);
+                }
+                curr[prevLen] = len;
+            }
+            next = curr;
+        }
+        return next[0];
+    }
+
 public:
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n, -1);
-        
-        dp[0] = 1;
-        for(int i=1; i<n; i++){
-            dp[i] = 1;
-            for(int j=i-1; j>=0; j--){
-                if(nums[j]>=nums[i]){
-                    continue;
-                }
-                int possibleAns = dp[j]+1;
-                dp[i] = max(dp[i], possibleAns);
-            }
-        }
-        
-        int ans = 0;
-        for(int i=0;i<n;i++){
-            ans = max(ans, dp[i]);
-        }
-        return ans;
+        return lis2(n, nums);
     }
 };
