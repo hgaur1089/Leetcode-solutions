3fc1acbfd35a3afc488f33577aa39401b670a02c
hgaur1089 <60226963+hgaur1089@users.noreply.github.com>
2022-09-02 21:34:08 +0530

    Time: 3 ms (48.38%), Space: 5.9 MB (46.23%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
-    double pos(double x, int n){
-        if(n == 0) 
-            return 1;
-        
-        double temp = pos(x, n/2);
-        if(n%2 == 0){
-            return temp*temp;
-        } else {
-            if(n > 0)
-                return x * temp * temp;
-            else 
-                return (temp * temp)/x;
-        }
-    }
-    
     double myPow(double x, int n) {
-        double ans;
-        ans = pos(x, n);
+        double ans = 1.0;
+        long long nn = n;
+        if(nn<0) nn = -1 * nn;
+        
+        while(nn){
+            if(nn%2==1){
+                ans = ans*x;
+                nn = nn-1;
+            } else {
+                x = x*x;
+                nn = nn/2;
+            }
+        }
+        
+        if(n<0) ans = (double)1.0 / (double)ans;
         return ans;
     }
 };
