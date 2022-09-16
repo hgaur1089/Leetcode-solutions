a885dfe587055f527dca38413ecba275b6778ab2
hgaur1089 <60226963+hgaur1089@users.noreply.github.com>
2022-09-16 22:36:57 +0530

    Time: 12 ms (90.18%), Space: 10.9 MB (46.66%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -1,44 +1,17 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-//         // int a, b;
-//         vector<int> ans;
-//         // for(int i=0; i<nums.size(); i++){
-//         //     a = i;
-//         //     for(int j=i+1; j<nums.size(); j++){
-//         //         b = j;
-//         //         if(nums[i]+nums[j] == target){
-//         //              ans.push_back(a);
-//         //              ans.push_back(b);
-//         //              return ans;
-//         //         }
-//         //     }
-//         // }
-        
-//         unordered_map<int, int> arr;
-//         for(int i=0; i<nums.size(); i++){
-//             int comp = target - nums[i];
-//             auto x = arr.find(comp);
-//             if(x != arr.end()){
-//                 ans.push_back(x->second);
-//                 ans.push_back(i);
-//             }
-            
-//             arr[nums[i]] = i;
-//         }
-        
-//         return ans;
+        unordered_map<int, int> hash;
         
         vector<int> ans;
-        unordered_map<int, int> map;
-        for(int i=0; i<nums.size(); i++){
-            if(map.count(target - nums[i]) > 0){
+        for(int i=0;i<nums.size();i++){
+            if(hash.find(target-nums[i])!=hash.end()){
                 ans.push_back(i);
-                ans.push_back(map[target - nums[i]]);
+                ans.push_back(hash[target-nums[i]]);
                 return ans;
             }
             
-            map[nums[i]] = i;
+            hash[nums[i]] = i;
         }
         
         return ans;
