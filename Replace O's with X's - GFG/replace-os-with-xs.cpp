49317e3ec7c6e4362ec786213a4b846f6e6ef58d
hgaur1089 <60226963+hgaur1089@users.noreply.github.com>
2023-01-03 23:04:34 +0530

    Added solution - LeetHub

---
+++ Replace O's with X's - GFG/replace-os-with-xs.cpp
@@ -0,0 +1,85 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+    void dfs(int row, int col, vector<vector<char>>& mat, vector<vector<int>>& vis){
+        vis[row][col] = 1;
+        
+        int delrow[] = {-1, 0, +1, 0};
+        int delcol[] = {0, +1, 0, -1};
+        for(int i=0; i<4; i++){
+            int nrow = row + delrow[i];
+            int ncol = col + delcol[i];
+            
+            if(nrow>=0 && nrow<mat.size() && ncol>=0 && ncol <mat[0].size() &&
+                mat[nrow][ncol] == 'O' && !vis[nrow][ncol]){
+                dfs(nrow, ncol, mat, vis);
+            }
+        }
+    }
+public:
+    vector<vector<char>> fill(int n, int m, vector<vector<char>> mat)
+    {
+        // code here
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+        
+        for(int i=0; i<n; i++){
+            if(!vis[i][0] && mat[i][0] == 'O'){
+                dfs(i, 0, mat, vis);
+            }
+            if(!vis[i][m-1] && mat[i][m-1] == 'O'){
+                dfs(i, m-1, mat, vis);
+            }
+        }
+        for(int j=0;j<m;j++){
+            if(!vis[0][j] && mat[0][j] == 'O'){
+                dfs(0, j, mat, vis);
+            }
+            if(!vis[n-1][j] && mat[n-1][j] == 'O'){
+                dfs(n-1, j, mat, vis);
+            }
+        }
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(mat[i][j]=='O' && !vis[i][j]){
+                    mat[i][j] = 'X';
+                }
+            }
+        }
+        
+        return mat;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n, m;
+        cin>>n>>m;
+        vector<vector<char>> mat(n, vector<char>(m, '.'));
+        for(int i = 0;i < n;i++)
+            for(int j=0; j<m; j++)
+                cin>>mat[i][j];
+        
+        Solution ob;
+        vector<vector<char>> ans = ob.fill(n, m, mat);
+        for(int i = 0;i < n;i++) {
+            for(int j = 0;j < m;j++) {
+                cout<<ans[i][j]<<" ";
+            }
+            cout<<"\n";
+        }
+    }
+    return 0;
+}
+// } Driver Code Ends
